066e2d5f244deae94f09466a0f916a47e0549f54
Callum <callumj22@gmail.com>
2021-03-28 23:43:59 +0100

    Created folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
       <h1>SVOIP</h1>
       <h2>Humble in the streets; Swag in the sheets</h2>
+        <Home/>
     </div>
   );
 }
