6ea9b27817adddfe3a4a1c60fe2946972b4fe135
Callum <callumj22@gmail.com>
2021-03-28 23:16:04 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SVOIP</h1>
+      <h2>Humble in the streets; Swag in the sheets</h2>
+    </div>
+  );
+}
+
+export default App;
